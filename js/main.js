fcdf0b5f5f94463d428eb4a0b31d5470aa7ddf46
Shane Taylor <shaneallantaylor@gmail.com>
2015-05-07 17:25:41 -0700

    second question details added

--- js/main.js
+++ js/main.js
@@ -4,24 +4,32 @@ $(document).ready(function(){
 // dope!
   $(".household-size").keyup(function() {
     if ($('.household-size').length > 0 && $('.household-size').val() != '') {
-      $('.button-container').removeClass('nope');
       $('.continue').removeClass('nope');
     }
     else {
-      $('.button-container').addClass('nope');
       $('.continue').addClass('nope');
     }
   });
 
   $('.freq').change(function(){
-      if ($('.freq').val() != '') {
+      if ($('.freq').val() != 'hour') {
         $('.button-container').removeClass('nope');
         $('.continue').removeClass('nope');
       }
-      else {
-        $('.button-container').addClass('nope');
-        $('.continue').addClass('nope');
+      else if ($('.freq').val() == 'hour') {
+        $('.specific').removeClass('nope');
       }
+      else {
+      }
+  });
+
+  $(".hours").keyup(function() {
+    if ($('.hours').length > 0 && $('.hours').val() != '') {
+      $('.continue').removeClass('nope');
+    }
+    else {
+      $('.continue').addClass('nope');
+    }
   });
 
 // click listener for continue button
