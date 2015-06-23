8324e5d32b826eb8355bdae1e9b9ed348e68126d
Shane Taylor <shaneallantaylor@gmail.com>
2015-06-23 15:47:35 -0700

    a few bugs to debug but it works mostly

--- js/main.js
+++ js/main.js
@@ -5,30 +5,112 @@ $(document).ready(function(){
   var FREQ_SELECTION;
   var FREQ_MULTIPLIER;
   var WEEKLY_HOURS = 1;
+  var YEARLY_GROSS;
+  var MONTHLY_GROSS;
+  var RENT_SUGGESTION;
+  var LA_MFI;
+  var USER_MFI;
+  var MFI_ROUNDED_UP;
 
-  function calcPayMulti(i) {
+  function calcFreqMulti(i) {
     switch (i) {
       case 'year':
         FREQ_MULTIPLIER = 1;
-      ;
+      break;
 
       case 'month':
         FREQ_MULTIPLIER = 12;
-      ;
+      break;
 
       case 'twiceamonth':
         FREQ_MULTIPLIER = 24;
-      ;
+      break;
 
       case 'twoweeks':
         FREQ_MULTIPLIER = 26;
-      ;
+      break;
 
       case 'week':
         FREQ_MULTIPLIER = 52;
-      ;
+      break;
+
+      case 'hour':
+        FREQ_MULTIPLIER = 52;
+      break;
     }
   };
+
+  function doRentSuggestionMath(i) {
+    YEARLY_GROSS = HOW_MUCH * FREQ_MULTIPLIER * WEEKLY_HOURS;
+    MONTHLY_GROSS = YEARLY_GROSS/12;
+    RENT_SUGGESTION = (MONTHLY_GROSS/3).toFixed([0]);
+
+    switch (true) {
+      case (i == 1):
+        LA_MFI = 58100;
+      break;
+
+      case (i == 2):
+        LA_MFI = 66400;
+      break;
+
+      case (i == 3):
+        LA_MFI = 74700;
+      break;
+
+      case (i == 4):
+        LA_MFI = 83000;
+      break;
+
+      case (i == 5):
+        LA_MFI = 89700;
+      break;
+
+      case (i == 6):
+        LA_MFI = 96300;
+      break;
+
+      case (i == 7):
+        LA_MFI = 103000;
+      break;
+
+      case (i >= 8):
+        LA_MFI = 109600;
+      break;
+    }
+
+    USER_MFI = ((YEARLY_GROSS/LA_MFI) * 100).toFixed([0]);
+
+    switch (true) {
+      case (USER_MFI <= 30):
+        MFI_ROUNDED_UP = 30;
+      break;
+
+      case (USER_MFI > 30 && USER_MFI <= 40):
+        MFI_ROUNDED_UP = 40;
+      break;
+
+      case (USER_MFI > 40 && USER_MFI <= 50):
+        MFI_ROUNDED_UP = 50;
+      break;
+
+      case (USER_MFI > 50 && USER_MFI <= 60):
+        MFI_ROUNDED_UP = 60;
+      break;
+
+      case (USER_MFI > 60):
+        MFI_ROUNDED_UP = 100;
+      break;
+    }
+
+    if (MFI_ROUNDED_UP <= 60) {
+      $('.mfi-exp').removeClass('hidden');
+    }
+
+    $('.search-link').attr('href', "https://losangeles.craigslist.org/search/apa?maxAsk=" + RENT_SUGGESTION);
+
+  };
+
 
   // displays the "continue" button after user keys up while in input field
   // dope!
@@ -46,7 +128,6 @@ $(document).ready(function(){
       if ($('.freq').val() != 'hour') {
         $('.button-container').removeClass('nope');
         $('#cont2').removeClass('nope');
-        console.log('Cont2 is SHOWN!');
       }
       else if ($('.freq').val() == 'hour') {
         $('.specific').removeClass('nope');
@@ -56,7 +137,6 @@ $(document).ready(function(){
   $(".hours").keyup(function() {
     if ($('.hours').length > 0 && $('.hours').val() != '') {
       $('#cont3').removeClass('nope');
-      console.log('Cont3 is SHOWN!');
     }
     else {
       $('#cont3').addClass('nope');
@@ -71,7 +151,6 @@ $(document).ready(function(){
 
   $('#cont1').click(function() {
     FAMILY_SIZE = $('.household-size').val();
-    console.log(FAMILY_SIZE);
     $('.progress').css("left", "-33%");
     $('.q1').addClass('hidden');
     $('.q2').removeClass('hidden');
@@ -80,15 +159,21 @@ $(document).ready(function(){
 
   $('#cont2').click(function() {
     HOW_MUCH = $('.income').val();
-    console.log(HOW_MUCH);
     FREQ_SELECTION = $('.freq').val();
-    console.log(FREQ_MULTIPLIER);
+    calcFreqMulti(FREQ_SELECTION);
+    doRentSuggestionMath(FAMILY_SIZE);
     $('.progress').css("left", "-0%");
     $('.q2').addClass('hidden');
     $('.sub-title').addClass('hidden');
     $('.results').removeClass('nope');
     $('.details').removeClass('nope');
-    $('.monthly-rent-suggestion').append('$' + HOW_MUCH);
+    $('.mfi-status').removeClass('nope');
+    $('.search-county').removeClass('nope');
+    $('.monthly-rent-suggestion').append('$' + RENT_SUGGESTION);
+    $('.mfi').append(USER_MFI);
+    $('.mfi-rounded-up').append(MFI_ROUNDED_UP);
+
+
     // DO CALCULATION (exclude hours per week) and
     // show result
 
@@ -101,12 +186,18 @@ $(document).ready(function(){
       $('.sub-title').addClass('hidden');
       $('.results').removeClass('nope');
       $('.details').removeClass('nope');
-      $('.monthly-rent-suggestion').append('$800');
+      $('.mfi-status').removeClass('nope');
+      $('.search-county').removeClass('nope');
+      HOW_MUCH = $('.income').val();
+      FREQ_SELECTION = $('.freq').val();
+      WEEKLY_HOURS = $('.hours').val();
+      calcFreqMulti(FREQ_SELECTION);
+      doRentSuggestionMath(FAMILY_SIZE);
+      $('.monthly-rent-suggestion').append('$' + RENT_SUGGESTION);
+      $('.mfi').append(USER_MFI);
+      $('.mfi-rounded-up').append(MFI_ROUNDED_UP);
       // DO CALCULATION (include hours per week) and
       // show result
-      console.log(HOW_MUCH);
-      PAY_FREQ = $('.freq').val();
-      console.log(PAY_FREQ);
   });
 
 });
